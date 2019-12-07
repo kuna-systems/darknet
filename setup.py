9489aae47f6123c83246597e7cd8576be928dee6
Alexander Wilson <awlego@gmail.com>
2019-12-06 16:02:50 -0800

    KAI-181 example of building a pip package with darknet

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+# from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="darknet",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
